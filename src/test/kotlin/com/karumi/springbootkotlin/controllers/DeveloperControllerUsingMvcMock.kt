50582b49b3d7f28e1213b1e05c8b3f78d2257b03
tonilopezmr <tonilopezmr@gmail.com>
2019-02-18 17:43:28 +0100

    Use spec

--- src/test/kotlin/com/karumi/springbootkotlin/controllers/DeveloperControllerUsingMvcMock.kt
+++ src/test/kotlin/com/karumi/springbootkotlin/controllers/DeveloperControllerUsingMvcMock.kt
@@ -9,9 +9,7 @@ import com.karumi.springbootkotlin.given.GivenDeveloper
 import com.karumi.springbootkotlin.given.GivenDeveloper.Companion.DEVELOPER_ID
 import com.ninjasquad.springmockk.MockkBean
 import io.kotlintest.Spec
-import io.kotlintest.TestContext
-import io.kotlintest.extensions.TestListener
-import io.kotlintest.specs.StringSpec
+import io.kotlintest.specs.WordSpec
 import io.mockk.every
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
@@ -32,7 +30,7 @@ import java.nio.file.Files
 @RunWith(SpringRunner::class)
 @SpringBootTest
 @AutoConfigureMockMvc
-class DeveloperControllerUsingMvcMock : StringSpec(), GivenDeveloper {
+class DeveloperControllerUsingMvcMock : WordSpec(), GivenDeveloper {
 
   @Autowired
   private lateinit var mockMvc: MockMvc
@@ -40,77 +38,73 @@ class DeveloperControllerUsingMvcMock : StringSpec(), GivenDeveloper {
   @MockkBean
   private lateinit var developerDao: DeveloperDao
 
-  object Newlistener : TestListener {
-
-    override fun beforeSpec(spec: Spec) {
-      try {
-        TestContextManager(spec.javaClass).prepareTestInstance(spec)
-      } catch (t: Throwable) {
-        t.printStackTrace()
-      }
-    }
+  // Use this until the next release 3.3.0: https://github.com/kotlintest/kotlintest/pull/684
+  override fun beforeSpec(spec: Spec) {
+    TestContextManager(this.javaClass).prepareTestInstance(spec)
   }
 
-  override fun listeners() = listOf(Newlistener)
-
   init {
-    "developer POST should create a developer if it's a karumi developer" {
-      val createdDeveloper = givenNewKarumiDeveloper().copy(id = GivenDeveloper.DEVELOPER_ID)
+    "POST /developer" should {
+      "create a developer if it's a karumi developer" {
+        val createdDeveloper = givenNewKarumiDeveloper().copy(id = GivenDeveloper.DEVELOPER_ID)
 
-      every { developerDao.create(any()) } returns createdDeveloper.success()
+        every { developerDao.create(any()) } returns createdDeveloper.success()
 
-      mockMvc.perform(
-        post("/developer")
-          .contentType(MediaType.APPLICATION_JSON)
-          .content(getContent("CreateKarumiDeveloper.json"))
-      ).andExpect(status().isCreated)
-        .andDo(print())
-        .andReturn()
-        .matchWithSnapshot(this)
+        mockMvc.perform(
+          post("/developer")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(getContent("CreateKarumiDeveloper.json"))
+        ).andExpect(status().isCreated)
+          .andDo(print())
+          .andReturn()
+          .matchWithSnapshot(this)
+      }
+
+      "not create a developer if it isn't a karumi developer" {
+        mockMvc.perform(
+          post("/developer")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(getContent("CreateDeveloper.json"))
+        ).andExpect(status().isBadRequest)
+          .andDo(print())
+          .andReturn()
+      }
+
+      "returns 400 if the json body isn't expected" {
+        mockMvc.perform(
+          post("/developer")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(getContent("BadDeveloperBody.json"))
+        ).andExpect(status().isBadRequest)
+          .andDo(print())
+          .andReturn()
+      }
     }
 
-    "developer POST shouldn't create a developer if it isn't a karumi developer" {
-      mockMvc.perform(
-        post("/developer")
-          .contentType(MediaType.APPLICATION_JSON)
-          .content(getContent("CreateDeveloper.json"))
-      ).andExpect(status().isBadRequest)
-        .andDo(print())
-        .andReturn()
-    }
+    "GET /developer/{id}" should {
+      "retrieve by id" {
+        val createdDeveloper = givenNewKarumiDeveloper().copy(id = GivenDeveloper.DEVELOPER_ID)
+        every { developerDao.getById(DEVELOPER_ID) } returns createdDeveloper.some().success()
 
-    "developer POST should returns 400 if the json body isn't expected" {
-      mockMvc.perform(
-        post("/developer")
-          .contentType(MediaType.APPLICATION_JSON)
-          .content(getContent("BadDeveloperBody.json"))
-      ).andExpect(status().isBadRequest)
-        .andDo(print())
-        .andReturn()
-    }
+        mockMvc.perform(
+          get("/developer/$DEVELOPER_ID")
+            .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isOk)
+          .andDo(print())
+          .andReturn()
+          .matchWithSnapshot(this)
+      }
 
-    "developer GET should retrieve by id" {
-      val createdDeveloper = givenNewKarumiDeveloper().copy(id = GivenDeveloper.DEVELOPER_ID)
-      every { developerDao.getById(DEVELOPER_ID) } returns createdDeveloper.some().success()
+      "returns 404 code if there isn't the developer in the database" {
+        every { developerDao.getById(DEVELOPER_ID) } returns None.success()
 
-      mockMvc.perform(
-        get("/developer/$DEVELOPER_ID")
-          .contentType(MediaType.APPLICATION_JSON)
-      ).andExpect(status().isOk)
-        .andDo(print())
-        .andReturn()
-        .matchWithSnapshot(this)
-    }
-
-    "developer GET should returns 404 code if there isn't the developer in the database" {
-      every { developerDao.getById(DEVELOPER_ID) } returns None.success()
-
-      mockMvc.perform(
-        get("/developer/${GivenDeveloper.DEVELOPER_ID}")
-          .contentType(MediaType.APPLICATION_JSON)
-      ).andExpect(status().isNotFound)
-        .andDo(print())
-        .andReturn()
+        mockMvc.perform(
+          get("/developer/${GivenDeveloper.DEVELOPER_ID}")
+            .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(status().isNotFound)
+          .andDo(print())
+          .andReturn()
+      }
     }
   }
 
@@ -119,7 +113,7 @@ class DeveloperControllerUsingMvcMock : StringSpec(), GivenDeveloper {
     return String(Files.readAllBytes(resource.toPath()))
   }
 
-  fun MvcResult.matchWithSnapshot(context: TestContext) {
+  fun MvcResult.matchWithSnapshot(context: FinalTestContext) {
     response.contentAsString.matchWithSnapshot(context.description().name)
   }
 }
