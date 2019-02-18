34b37dea46cfec7f4a117ce9a67c6f02946ec310
tonilopezmr <tonilopezmr@gmail.com>
2019-02-18 03:10:05 +0100

    Add actuator server and integrate it with developer api server

---
+++ admin-server/src/main/kotlin/com/karumi/admin/server/AdminServerApplication.kt
@@ -0,0 +1,44 @@
+package com.karumi.admin.server
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository
+
+@SpringBootApplication
+@EnableAdminServer
+class AdminServerApplication
+
+fun main(args: Array<String>) {
+  runApplication<AdminServerApplication>(*args)
+}
+
+@Configuration
+class SecurityConfig : WebSecurityConfigurerAdapter() {
+
+  @Throws(Exception::class)
+  override fun configure(http: HttpSecurity) {
+    val successHandler = SavedRequestAwareAuthenticationSuccessHandler()
+    successHandler.setTargetUrlParameter("redirectTo")
+    successHandler.setDefaultTargetUrl("/")
+
+    http.authorizeRequests()
+        .antMatchers("/assets/**").permitAll()
+        .antMatchers("/login").permitAll()
+        .anyRequest().authenticated()
+        .and()
+        .formLogin().loginPage("/login").successHandler(successHandler).and()
+        .logout().logoutUrl("/logout").and()
+        .httpBasic().and()
+        .csrf()
+        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+        .ignoringAntMatchers(
+            "/instances",
+            "/actuator/**"
+        )
+  }
+}
