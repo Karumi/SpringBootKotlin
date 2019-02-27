e3b130a7da6c11096be55a7dd322f58da223a0a4
tonilopezmr <tonilopezmr@gmail.com>
2019-02-27 07:19:42 +0100

    Add worker and async code

--- src/main/kotlin/com/karumi/springbootkotlin/Application.kt
+++ src/main/kotlin/com/karumi/springbootkotlin/Application.kt
@@ -2,9 +2,28 @@ package com.karumi.springbootkotlin
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.scheduling.annotation.EnableAsync
+import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
+import java.util.concurrent.Executor
 
 @SpringBootApplication
-class Application
+@EnableAsync
+@EnableScheduling
+class Application {
+
+  @Bean
+  fun taskExecutor(): Executor {
+    val executor = ThreadPoolTaskExecutor()
+    executor.corePoolSize = 2
+    executor.maxPoolSize = 2
+    executor.setQueueCapacity(500)
+    executor.setThreadNamePrefix("Scheduler-")
+    executor.initialize()
+    return executor
+  }
+}
 
 fun main(args: Array<String>) {
   runApplication<Application>(*args)
--- src/main/kotlin/com/karumi/springbootkotlin/developers/api/DeveloperController.kt
+++ src/main/kotlin/com/karumi/springbootkotlin/developers/api/DeveloperController.kt
@@ -8,6 +8,7 @@ import com.karumi.springbootkotlin.developers.domain.usecase.CreateKarumiDevelop
 import com.karumi.springbootkotlin.developers.domain.usecase.GetDeveloper
 import org.springframework.http.HttpStatus.CREATED
 import org.springframework.http.ResponseEntity
+import org.springframework.scheduling.annotation.Async
 import org.springframework.security.core.Authentication
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -15,6 +16,9 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 import java.util.UUID
+import java.util.concurrent.CompletableFuture
+import java.util.concurrent.CompletionStage
+import java.util.logging.Logger
 
 @RestController
 class DeveloperController(
@@ -24,7 +28,14 @@ class DeveloperController(
 ) {
 
   @GetMapping("/")
-  fun index(): String = "Karumi"
+  fun index(): CompletionStage<String> = asyncHello()
+
+  @Async
+  fun asyncHello(): CompletionStage<String> {
+    Logger.getLogger(DeveloperController::class.qualifiedName).info("Karumi")
+    Thread.sleep(1000)
+    return CompletableFuture.completedFuture("Karumi")
+  }
 
   @PostMapping("/developer")
   fun createDeveloper(
