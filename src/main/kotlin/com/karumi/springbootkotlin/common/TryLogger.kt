7df19b68404289f4fc8e7b9aaefe3b025fd790ae
tonilopezmr <tonilopezmr@gmail.com>
2019-03-13 22:23:51 +0100

    Log refactor

--- src/main/kotlin/com/karumi/springbootkotlin/common/TryLogger.kt
+++ src/main/kotlin/com/karumi/springbootkotlin/common/TryLogger.kt
@@ -5,5 +5,8 @@ import java.util.logging.Level
 import java.util.logging.Logger
 
 inline fun <reified A : Any, B> A.TryLogger(f: () -> B): Try<B> = Try(f).apply {
-  fold({ Logger.getLogger(A::class.qualifiedName).log(Level.SEVERE, it.message, it) }, { Unit })
+  fold(
+    { Try { Logger.getLogger(A::class.qualifiedName).log(Level.SEVERE, it.message, it) } },
+    { Unit }
+  )
 }
